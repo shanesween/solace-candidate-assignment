6c9eda44f8d71ccd32526cc6d24d6b00e86108ae
Shane Sweeney <shane@bluepineapp.com>
2025-08-20 23:05:07 -0700

    Implement advocates query hook

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,15 @@ export interface ApiResponse<T> {
     data: T;
     error?: string;
 }
+
+export interface Advocate {
+    id: number;
+    firstName: string;
+    lastName: string;
+    city: string;
+    degree: string;
+    specialties: string[];
+    yearsOfExperience: number;
+    phoneNumber: number;
+    createdAt: string;
+}
